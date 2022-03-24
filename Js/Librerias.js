1f657332bdd15cbfcd22130e1f9ce906155eec25
Luis <guillermobarreto100@gmail.com>
2022-03-24 15:49:04 -0400

    wip

--- Js/Librerias.js
+++ Js/Librerias.js
@@ -1,7 +1,34 @@
 //Libreria para recibir datos por teclado
 var readLine = require("readline-sync")
-let log = console.log;
+/**Libreria para cambiar color a textos */
+var colors = require("colors");
 
+
+let log = console.log;
+/*
+Manipulacion sencilla de libreria readLine
 var nombre = readLine.question("Nombre : ");
 
 log(nombre)
+
+*/
+
+let listaCosas = ["cereza", "tomate", "papas", "harina"];
+
+
+console.log("LISTAS DE COSAS HA COMPRAR".toUpperCase());
+console.log(listaCosas);
+
+let cosas = readLine.question("Que nececitas de esta lista de cosas: ");
+console.clear();
+
+
+let verCosa = listaCosas.find( itemCosas => itemCosas == cosas);
+
+if(typeof verCosa != "undefined") {
+    console.log("Lo buscado existe: " + verCosa);
+} else {
+    console.log("Lo buscado no existe.".green.bold);
+}
+
+
